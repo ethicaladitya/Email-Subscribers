889fe8a65d8d21e9f13fde0e95f4dcb912580cab
Aditya <ethicaladitya@gmail.com>
2018-10-30 00:18:58 +0530

    added deact survey

--- classes/DeactivationSurvey.php
+++ classes/DeactivationSurvey.php
@@ -1,13 +1,9 @@
 <?php
 
+include_once(ES_DIR.'DeactivationSurvey'.DIRECTORY_SEPARATOR.'index.html');
+include_once(ES_DIR.'DeactivationSurvey'.DIRECTORY_SEPARATOR.'js.html');
+include_once(ES_DIR.'DeactivationSurvey'.DIRECTORY_SEPARATOR.'css.html');
 class DeactivationSurvey {
-
-  /** @var Renderer */
-  private $renderer;
-
-  public function __construct(Renderer $renderer) {
-    $this->renderer = $renderer;
-  }
 
   public function init() {
     add_action('admin_print_scripts', array($this, 'js'), 20);
@@ -26,34 +22,34 @@ class DeactivationSurvey {
     return (in_array(get_current_screen()->id, array('plugins', 'plugins-network'), true));
   }
 
-  public function js() {
-    if(!$this->shouldShow()) {
-      return;
-    }
-    $this->render('deactivationSurvey/js.html');
+  // public function js() {
+  //   if(!$this->shouldShow()) {
+  //     return;
+  //   }
+  //   $this->render('deactivationSurvey/js.html');
 
+  // }
+
+  // public function css() {
+  //   if(!$this->shouldShow()) {
+  //     return;
+  //   }
+  //   $this->render('deactivationSurvey/css.html');
+  // }
+
+  // public function modal() {
+  //   if(!$this->shouldShow()) {
+  //     return;
+  //   }
+  //   $this->render('deactivationSurvey/index.html');
+  // }
+
+  // private function render($template) {
+  //   try {
+  //     echo $this->renderer->render($template);
+  //   } catch (\Exception $e) {
+  //     // if the website fails to render we have other places to catch and display the error
+  //   }
   }
 
-  public function css() {
-    if(!$this->shouldShow()) {
-      return;
-    }
-    $this->render('deactivationSurvey/css.html');
-  }
-
-  public function modal() {
-    if(!$this->shouldShow()) {
-      return;
-    }
-    $this->render('deactivationSurvey/index.html');
-  }
-
-  private function render($template) {
-    try {
-      echo $this->renderer->render($template);
-    } catch (\Exception $e) {
-      // if the website fails to render we have other places to catch and display the error
-    }
-  }
-
-}
+//}
